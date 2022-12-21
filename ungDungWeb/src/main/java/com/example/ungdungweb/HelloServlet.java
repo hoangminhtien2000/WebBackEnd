08217317472c988d8adaebf37548616dba1c9d11
hoangminhtien2000 <hoangminhtien2000@gmail.com>
2022-12-21 16:25:18 +0700

    dictionary

--- ungDungWeb/src/main/java/com/example/ungdungweb/HelloServlet.java
+++ ungDungWeb/src/main/java/com/example/ungdungweb/HelloServlet.java
@@ -2,29 +2,28 @@ package com.example.ungdungweb;
 
 import java.io.*;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/nowday")
-//@WebServlet(name = "LoginServlet", value = "/login")
+//@WebServlet(name = "nowday", value = "/nowday")
+//@WebServlet(name = "Login", value = "/login")
+//@WebServlet(name = "ConverterServlet", value = "/convert")
+@WebServlet(name = "TranslateServlet", urlPatterns = "/translate")
 public class HelloServlet extends HttpServlet {
-    //private String message;
 
-    public void init() {
-        //message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        Date today =new Date();
-        out.println("<h1 style=\"color: blue\">" + today + "</h1>");
-        out.println("</body></html>");
-    }
+//    @Override
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        Date today =new Date();
+//        out.println("<h1 style=\"color: blue\">" + today + "</h1>");
+//        out.println("</body></html>");
+//    }
 
 //    @Override
 //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -43,6 +42,53 @@ public class HelloServlet extends HttpServlet {
 //        writer.println("</html>");
 //    }
 
-    public void destroy() {
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        float rate = Float.parseFloat(request.getParameter("rate"));
+//        float usd = Float.parseFloat(request.getParameter("usd"));
+//
+//        float vnd = rate * usd;
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//        writer.println("<h1>Rate: " + rate + "</h1>");
+//        writer.println("<h1>USD: " + usd + "</h1>");
+//        writer.println("<h1>VND: " + vnd + "</h1>");
+//        writer.println("</html>");
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("knife", "dao");
+        dictionary.put("computer", "Máy tính");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+
 }
--- ungDungWeb/src/main/webapp/index.jsp
+++ ungDungWeb/src/main/webapp/index.jsp
@@ -1,14 +1,15 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>JSP - Hello World</title>
-</head>
-<body>
-<h1><%= "Hello World!" %>
-</h1>
-<a href="nowday">Now Day</a>
-</body>
+
+<%--<head>--%>
+<%--    <title>JSP - Hello World</title>--%>
+<%--</head>--%>
+<%--<body>--%>
+<%--<h1><%= "Hello World!" %>--%>
+<%--</h1>--%>
+<%--<a href="nowday">Now Day</a>--%>
+<%--</body>--%>
 
 <%--<style type="text/css">--%>
 <%--    .login {--%>
@@ -31,5 +32,33 @@
 <%--    </div>--%>
 <%--</form>--%>
 <%--</body>--%>
+
+<%--<head>--%>
+<%--    <title>Currency Converter</title>--%>
+<%--    <link rel="stylesheet" type="text/css" href="css/style.css">--%>
+<%--</head>--%>
+<%--<body>--%>
+<%--<h2>Currency Converter</h2>--%>
+<%--<form action="/convert" method="post">--%>
+<%--    <label>Rate: </label><br/>--%>
+<%--    <input type="text" name="rate" placeholder="RATE" value="22000"/><br/>--%>
+<%--    <label>USD: </label><br/>--%>
+<%--    <input type="text" name="usd" placeholder="USD" value="0"/><br/>--%>
+<%--    <input type = "submit" id = "submit" value = "Converter"/>--%>
+<%--</form>--%>
+<%--</body>--%>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+    <title>Simple Dictionary</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<h2>Vietnamese Dictionary</h2>
+<form action="/translate" method="post">
+    <input type="text" name="txtSearch" placeholder="Enter your word: "/>
+    <input type = "submit" id = "submit" value = "Search"/>
+</form>
+</body>
 
 </html>
